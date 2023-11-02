779f06b34e6a8bb3d7d4a04ce4726c6ce9ffae6d
andresilva85 <andrefsilva.contato@gmail.com>
2023-11-02 19:08:51 -0300

    refactoring cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -8,14 +8,3 @@ module.exports = defineConfig({
     },
   },
 });
-
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      allureWriter(on, config);
-      return config;
-    }
-  }
-});
