ee8f6f1a3ac02c9a15338ebbcbf7b988cd06dc36
andresilva85 <andrefsilva.contato@gmail.com>
2023-11-02 19:25:46 -0300

    testing CI/CD

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "jdpoad", // Adicione o projectId aqui
+  projectId: "jdpoad", 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
