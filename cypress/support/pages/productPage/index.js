511aa73788133b237828d49d8f3942f7ab7ef277
andresilva85 <andrefsilva.contato@gmail.com>
2023-11-02 23:29:28 -0300

    fixing error not.be.visible

--- cypress/support/pages/productPage/index.js
+++ cypress/support/pages/productPage/index.js
@@ -3,7 +3,7 @@ import { productPageElements } from './elements';
 // Function to verify product sorting
 export const verifyProductSort = (sortOption) => {
   cy.get(productPageElements.productSortContainer).select(sortOption);
-  cy.get(productPageElements.inventoryList).should('not.be.visible');
+  cy.get(productPageElements.inventoryList).should('be.visible');
   cy.get(productPageElements.inventoryItemNames).should('be.visible')
 
 };
